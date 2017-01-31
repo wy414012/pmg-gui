ac6617dacfacc1a7a920cd19021e43d614be3ef4
Dietmar Maurer <dietmar@proxmox.com>
2017-01-31 17:33:33 +0100

    add syslog

--- js/ServerAdministration.js
+++ js/ServerAdministration.js
@@ -20,6 +20,12 @@ Ext.define('PMG.ServerAdministration', {
 	    html: "Server Administration2"
 	},
 	{
+	    xtype: 'proxmoxLogView',
+	    title: gettext('Syslog'),
+	    url: "/api2/extjs/nodes/" + Proxmox.NodeName + "/syslog",
+	    log_select_timespan: 1
+	},
+	{
 	    xtype: 'proxmoxNodeTasks',
 	    title: gettext('Tasks'),
 	    height: 'auto',
