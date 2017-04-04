ca6f3037f6702735acab7c8092e520cd169b1f08
Dominik Csapak <d.csapak@proxmox.com>
2017-04-04 13:48:46 +0200

    add versioninfo component
    this gets the current version from the api and displays it
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ js/VersionInfo.js
@@ -0,0 +1,32 @@
+Ext.define('PMG.view.main.VersionInfo',{
+    extend: 'Ext.Component',
+    xtype: 'versioninfo',
+
+    makeApiCall: true,
+
+    data: {
+	version: false
+    },
+
+    tpl: [
+	'Mail Gateway',
+	'<tpl if="version">',
+	' {version}-{release}/{repoid}',
+	'</tpl>'
+    ],
+
+    initComponent: function() {
+	var me = this;
+	me.callParent();
+
+	if (me.makeApiCall) {
+	    Proxmox.Utils.API2Request({
+		url: '/version',
+		method: 'GET',
+		success: function(response) {
+		    me.update(response.result.data);
+		}
+	    });
+	}
+    }
+});
