d9c3552acf01a9c498bee17e065c3219449956a8
Dietmar Maurer <dietmar@proxmox.com>
2017-08-10 14:00:44 +0200

    implement spam quarantine list and preview

--- js/SpamQuarantine.js
+++ js/SpamQuarantine.js
@@ -1,24 +1,193 @@
-Ext.define('PMG.SpamQuarantine', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'pmgSpamQuarantine',
+Ext.define('pmg-spam-archive', {
+    extend: 'Ext.data.Model',
+    fields: [
+	{ type: 'number', name: 'spamavg' },
+	{ type: 'integer', name: 'count' },
+        { type: 'date', dateFormat: 'timestamp', name: 'day' }
+    ],
+    proxy: {
+        type: 'proxmox',
+        url: "/api2/json/quarantine/spam"
+    },
+    idProperty: 'day'
+});
 
-    title: gettext('Spam Quarantine') + ': ' + Proxmox.UserName,
-    border: false,
-    scrollable: true,
-    defaults: { border: false },
+Ext.define('PMG.SpamArchive', {
+    extend: 'Ext.grid.GridPanel',
+    xtype: 'pmgSpamArchive',
 
-    items: [
+    controller: {
+
+        xclass: 'Ext.app.ViewController',
+
+	init: function(view) {
+	    view.store.load({
+		callback: function() {
+		    view.getSelectionModel().select(0);
+		}
+	    });
+	}
+    },
+
+    store: {
+        model: 'pmg-spam-archive'
+    },
+
+    columns: [
+        {
+	    xtype: 'datecolumn',
+            header: gettext('Date'),
+	    format: 'Y-m-d',
+            dataIndex: 'day',
+	    flex: 1
+        },
 	{
-	    title: gettext('Today'),
-	    itemId: 'today',
-	    html: "Backup"
+	    header: gettext('Count'),
+	    dataIndex: 'count',
+	    flex: 1
 	},
 	{
-            title: gettext('Archive'),
-	    itemId: 'archive',
-	    html: "Archive"
+	    header: gettext('Spam level'),
+	    dataIndex: 'spamavg',
+	    flex: 1
 	}
     ]
 });
 
+Ext.define('pmg-spam-list', {
+    extend: 'Ext.data.Model',
+    fields: [ 'id', 'envelope_sender', 'from', 'sender', 'receiver', 'subject',
+	{ type: 'number', name: 'bytes' },
+	{ type: 'integer', name: 'bytes' },
+        { type: 'date', dateFormat: 'timestamp', name: 'time' }
+    ],
+    proxy: {
+        type: 'proxmox',
+    },
+    idProperty: 'id'
+});
 
+Ext.define('PMG.SpamList', {
+    extend: 'Ext.grid.GridPanel',
+    xtype: 'pmgSpamList',
+
+    setDay: function(day) {
+	var me = this;
+
+	me.setTitle(Ext.Date.format(day, "F j Y"));
+	me.store.load({
+            url: "/api2/json/quarantine/spam/" + (day.getTime() / 1000)
+	});
+    },
+
+    store: {
+	model: 'pmg-spam-list',
+    },
+
+    tbar: [
+	{
+	    text: gettext('Whitelist')
+	},
+	{
+	    text: gettext('Blacklist')
+	},
+	{
+	    text: gettext('Deliver')
+	},
+	{
+	    text: gettext('Delete')
+	}
+    ],
+
+    columns: [
+        {
+            header: gettext('Sender/Subject'),
+            dataIndex: 'subject',
+	    flex: 1
+        },
+	{
+	    header: gettext('Arrival Time'),
+	    dataIndex: 'time',
+	    flex: 1
+	}
+    ]
+});
+
+Ext.define('PMG.SpamQuarantine', {
+    extend: 'Ext.container.Container',
+    xtype: 'pmgSpamQuarantine',
+
+    border: false,
+    layout: 'border',
+
+    defaults: { border: false },
+
+    controller: {
+
+        xclass: 'Ext.app.ViewController',
+
+        init: function(view) {
+        },
+
+	onSelectDay: function() {
+            var view = this.getView();
+
+	    var grid = this.lookupReference('archive');
+	    var rec = grid.selModel.getSelection()[0];
+
+	    if (!rec || !rec.data || !rec.data.day) return;
+
+	    var spamlist = this.lookupReference('spamlist');
+	    spamlist.setDay(rec.data.day);
+	},
+
+	onSelectMail: function() {
+	    var spamlist = this.lookupReference('spamlist');
+	    var rec = spamlist.selModel.getSelection()[0];
+
+	    var preview = this.lookupReference('preview');
+
+	    if (!rec || !rec.data || !rec.data.id)  {
+		preview.update('');
+		return;
+	    }
+
+	    var url = '/api2/htmlmail/quarantine/content?id=' + rec.data.id;
+	    preview.update("<iframe frameborder=0 width=100% height=100% src='" + url +"'></iframe>");
+	},
+    },
+
+    items: [
+	{
+	    xtype: 'pmgSpamArchive',
+	    reference: 'archive',
+	    region: 'west',
+	    width: 320,
+	    split: true,
+
+	    listeners: {
+		selectionChange: 'onSelectDay'
+	    }
+	},
+	{
+	    xtype: 'panel',
+	    region: 'center',
+	    layout: { type: 'vbox', align: 'stretch' },
+	    items: [
+		{
+		    xtype: 'pmgSpamList',
+		    reference: 'spamlist',
+		    height: 300,
+		    listeners: {
+			selectionChange: 'onSelectMail'
+		    }
+		},
+		{
+		    split: true,
+		    flex: 1,
+		    reference: 'preview',
+		}
+	    ]
+	}
+    ]
+});
