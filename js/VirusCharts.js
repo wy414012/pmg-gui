db8f82ca4f585c7922f85dea4fad71d306c2d507
Dietmar Maurer <dietmar@proxmox.com>
2017-08-31 06:25:55 +0200

    add VirusCharts class

---
+++ js/VirusCharts.js
@@ -0,0 +1,42 @@
+Ext.define('PMG.VirusCharts', {
+    extend: 'Ext.grid.GridPanel',
+    xtype: 'pmgVirusCharts',
+
+    title: gettext('Virus Charts'),
+    
+    disableSelection: true,
+
+    tbar: [ { xtype: 'pmgStatTimeSelector' } ],
+
+    emptyText: gettext('No virus names in database.'),
+    viewConfig: {
+	deferEmptyText: false
+    },
+
+    store: {
+	xclass: 'PMG.data.StatStore',
+	fields: [ 'name', 'count' ],
+	staturl: "/api2/json/statistics/virus"
+    },
+
+    columns: [
+	{
+	    header: gettext('Name'),
+	    flex: 1,
+	    dataIndex: 'name'
+	},
+	{
+	    header: gettext('Count'),
+	    width: 150,
+	    dataIndex: 'count'
+	}
+    ],
+
+    initComponent: function() {
+	var me = this;
+	
+	me.callParent();
+	
+	Proxmox.Utils.monStoreErrors(me, me.store);
+    }
+});
