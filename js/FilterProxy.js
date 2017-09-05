2f5b0b8ae46416bf731e152f24f1d461b96d0be5
Dietmar Maurer <dietmar@proxmox.com>
2017-09-05 10:50:39 +0200

    FilterProxy.js - helper class

---
+++ js/FilterProxy.js
@@ -0,0 +1,26 @@
+Ext.define('PMG.FilterProxy', {
+    extend: 'Proxmox.RestProxy',
+    alias : 'proxy.pmgfilterproxy',
+
+    filterId: undefined, // 'x-gridfilter-XXXXX'
+
+    getParams: function(operation) {
+	var me = this, i;
+	if (!operation.isReadOperation) {
+	    return {};
+	}
+	var params = me.callParent(arguments);
+
+	var filters = operation.getFilters() || [];
+	for (i = 0; i < filters.length; i++) {
+	    filter = filters[i];
+	    if (filter.config.id === me.filterId) {
+		var v = filter.getValue();
+		if (v !== undefined && v !== '') {
+		    params.filter = v;
+		}
+	    }
+	}
+	return params;
+    }
+});
