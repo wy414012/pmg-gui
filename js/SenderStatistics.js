0dd929ecada5159576c91bba62228cf02d0303a2
Dietmar Maurer <dietmar@proxmox.com>
2017-09-05 10:22:00 +0200

    improve sender statistic panel

--- js/SenderStatistics.js
+++ js/SenderStatistics.js
@@ -1,16 +1,96 @@
-Ext.define('PMG.SenderStatistics', {
+Ext.define('PMG.SenderDetails', {
     extend: 'Ext.grid.GridPanel',
-    xtype: 'pmgSenderStatistics',
+    xtype: 'pmgSenderDetails',
+
+    dockedItems: [
+	{
+	    dock: 'top',
+	    xtype: 'panel',
+	    itemId: 'info',
+	    bodyPadding: 10,
+	    html: gettext('Please select a sender.')
+	}
+    ],
+
+    disableSelection: true,
+
+    setUrl: function(url, title) {
+	var me = this;
+
+	me.store.setUrl(url);
+
+	var infopanel = me.getComponent('info');
+	if (title) {
+	    infopanel.update(title);
+	} else {
+	    infopanel.update(gettext('Please select a sender.'));
+	}
+    },
+
+    store: {
+	type: 'pmgStatStore',
+	remoteSort: true,
+	fields: [
+	    'receiver', 'virusname',
+	    { type: 'integer', name: 'bytes' },
+	    { type: 'boolean', name: 'blocked' },
+	    { type: 'integer', name: 'spamlevel' },
+            { type: 'date', dateFormat: 'timestamp', name: 'time' }
+	],
+	proxy: {
+	    type: 'proxmox',
+	    sortParam: 'orderby'
+	},
+	sorters: [
+	    {
+		property: 'time',
+		direction: 'ASC'
+	    }
+	]
+    },
+
+    columns: [
+	{
+	    text: gettext('Receiver'),
+	    renderer: Ext.htmlEncode,
+	    flex: 1,
+	    dataIndex: 'receiver'
+	},
+	{
+	    header: gettext('Size') + ' (KB)',
+	    renderer: function(v) { return Ext.Number.toFixed(v/1024, 0); },
+	    dataIndex: 'bytes'
+	},
+	{
+	    xtype: 'datecolumn',
+	    header: gettext('Date'),
+	    format: 'Y-m-d',
+	    dataIndex: 'time'
+	},
+	{
+	    xtype: 'datecolumn',
+	    header: gettext('Time'),
+	    format: 'H:m:s',
+	    dataIndex: 'time'
+	},
+    ]
+});
+
+Ext.define('PMG.SenderList', {
+    extend: 'Ext.grid.GridPanel',
+    alias: 'widget.pmgSenderList',
 
     title: gettext('Statistics') + ': ' + gettext('Sender'),
 
-    tbar: [ { xtype: 'pmgStatTimeSelector' } ],
-
     multiColumnSort: true,
-
-    layout: 'fit',
-
     plugins: 'gridfilters',
+
+    emptyText: gettext('No data in database.'),
+    viewConfig: {
+	deferEmptyText: false
+    },
+
+    tbar: [ { xtype: 'pmgStatTimeSelector' } ],
 
     store: {
 	type: 'pmgStatStore',
@@ -66,13 +146,14 @@ Ext.define('PMG.SenderStatistics', {
 	{
 	    text: gettext('Sender'),
 	    flex: 1,
+	    renderer: Ext.htmlEncode,
 	    dataIndex: 'sender',
 	    filter: {
 		type: 'string',
 		itemDefaults: {
-                    // any Ext.form.field.Text configs accepted
+		    // any Ext.form.field.Text configs accepted
 		}
-            }
+	    }
 	},
 	{
 	    text: gettext('Count'),
@@ -84,6 +165,59 @@ Ext.define('PMG.SenderStatistics', {
 	    renderer: function(v) {
 		return Ext.Number.toFixed(v/1024, 0);
 	    }
+	}
+    ],
+
+    initComponent: function() {
+	var me = this;
+	me.callParent();
+
+	Proxmox.Utils.monStoreErrors(me, me.store);
+    }
+});
+
+Ext.define('PMG.SenderStatistics', {
+    extend: 'Ext.panel.Panel',
+    xtype: 'pmgSenderStatistics',
+
+    layout: 'border',
+    border: false,
+    defaults: {
+	border: false,
+    },
+
+    controller: {
+	xclass: 'Ext.app.ViewController',
+
+	selectionChange: function(grid, selected, eOpts) {
+	    var details =  this.lookupReference('details');
+	    if (selected.length > 0) {
+		var sender = selected[0].data.sender;
+		var url = "/api2/json/statistics/sender/" +
+		    encodeURIComponent(sender);
+		details.setUrl(url, '<b>' + gettext('Sender') + ':</b> ' + Ext.htmlEncode(sender));
+	    } else {
+		details.setUrl();
+	    }
+	}
+    },
+
+    items: [
+	{
+	    xtype: 'pmgSenderList',
+	    multiColumnSort: true,
+	    region: 'center',
+	    layout: 'fit',
+	    flex: 1,
+
+	    listeners: { selectionchange: 'selectionChange' },
+	},
+	{
+	    xtype: 'pmgSenderDetails',
+	    region: 'east',
+	    reference: 'details',
+	    split: true,
+	    flex: 1
 	}
     ]
 });
