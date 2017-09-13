f3aa84ddaa44de2e3204c91cac7836eff5d36919
Dominik Csapak <d.csapak@proxmox.com>
2017-09-13 15:10:34 +0200

    add new RegexTester component
    used for testing regexes in the what object editors
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ js/RegexTester.js
@@ -0,0 +1,71 @@
+Ext.define('PMG.RegexTester', {
+    extend: 'Ext.form.FieldContainer',
+    alias: 'widget.pmgRegexTester',
+
+    // the field reference which holds the regex value
+    // has to be a sibling of the RegexTester component
+    regexFieldReference: undefined,
+
+    // if true, wraps the regex with ^ and $
+    wholeMatch: false,
+
+    layout: 'hbox',
+    submitValue: false,
+
+    items: [{
+	xtype: 'textfield',
+	submitValue: false,
+	name: 'teststring',
+	isDirty: function () { return false; },
+	reset: Ext.emptyFn
+    },{
+	margin: '0 0 0 5',
+	xtype: 'button',
+	text: 'Test',
+	handler: function() {
+	    var me = this.up();
+	    var regexField = me.up().down('field[reference=' + me.regexFieldReference +']');
+	    var regex = '';
+
+	    if (me.wholeMatch) {
+		regex = '^' + regexField.getValue() + '$';
+	    } else {
+		regex = regexField.getValue();
+	    }
+
+	    Proxmox.Utils.API2Request({
+		url: '/api2/extjs/config/regextest',
+		waitMsgTarget: me.up('window'),
+		params: {
+		    regex: regex,
+		    text: me.down('textfield[name=teststring]').getValue()
+		},
+		method: 'POST',
+		success: function(response) {
+		    Ext.Msg.show({
+			title: gettext('Success'),
+			message: gettext('OK') +
+			    ' (elapsed time: ' +
+			    response.result.data + 'ms' + ')',
+			buttons: Ext.Msg.OK,
+			icon: Ext.MessageBox.INFO
+		    });
+		},
+		failure: function(response, opts) {
+		    Ext.Msg.alert(gettext('Error'), response.htmlStatus);
+		}
+	    });
+	}
+    }],
+
+    initComponent: function() {
+	var me = this;
+
+	if (!me.regexFieldReference) {
+	    throw "No regex field reference given";
+	}
+
+	me.callParent();
+
+    }
+});
