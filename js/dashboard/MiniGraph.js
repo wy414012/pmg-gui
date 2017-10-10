a0ff4ab728fac3db3b1f4dbc0676ecb60b787254
Dominik Csapak <d.csapak@proxmox.com>
2017-10-10 15:16:23 +0200

    add minigraph component
    this is a preconfigured RRDChart for the dashboard
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ js/dashboard/MiniGraph.js
@@ -0,0 +1,41 @@
+Ext.define('PMG.dashboard.MiniGraph', {
+    extend: 'Proxmox.widget.RRDChart',
+    xtype: 'pmgMiniGraph',
+
+    legend: undefined,
+    interactions: undefined,
+    insetPadding: 20,
+    padding: '5 5 0 0',
+    axes: [
+	{
+	    type: 'numeric',
+	    position: 'left',
+	    minimum: 0,
+	    grid: true,
+	    majorTickSteps: 2,
+	    label: {
+		fillStyle: '#5f5f5f'
+	    },
+	    style: {
+		axisLine: false,
+		majorTickSize: 0,
+	    },
+	},
+	{
+	    type: 'time',
+	    position: 'bottom',
+	    dateFormat: 'H:i',
+	    fields: ['time'],
+	    label: {
+		fillStyle: '#5f5f5f'
+	    },
+	    style: {
+		axisLine: false,
+		majorTickSize: 0,
+	    }
+	}
+    ],
+    border: false,
+    flex: 1,
+    noTool: true,
+});
