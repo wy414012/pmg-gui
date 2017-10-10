ffdf818d7141cb53c5101dde555cdb81065f45d7
Dominik Csapak <d.csapak@proxmox.com>
2017-10-10 15:16:24 +0200

    add new SubscriptionInfo component
    this shows the SubscriptionInfo status in the dashboard and with a
    click on it you get to the Subscription menu item
    
    Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

---
+++ js/dashboard/SubscriptionInfo.js
@@ -0,0 +1,53 @@
+Ext.define('PMG.dashboard.SubscriptionInfo', {
+    extend: 'Ext.panel.Panel',
+    xtype: 'pmgSubscriptionInfo',
+
+    data: {
+	icon: 'question-circle',
+	message: gettext('Unknown')
+    },
+
+    style: {
+	cursor: 'pointer'
+    },
+
+    setSubStatus: function(status) {
+	var me = this;
+	var data = {};
+
+	switch (status) {
+	    case 2:
+		data.icon = 'check green';
+		data.message = gettext('Your subscription status is valid.');
+		break;
+	    case 1: 
+		data.icon = 'exclamation-triangle yellow';
+		data.message = gettext('Warning: Your subscription levels are not the same.');
+		break;
+	    case 0: 
+		data.icon = 'times-circle red';
+		data.message = gettext('You have at least one node without subscription.');
+		break;
+	    default:
+		throw 'invalid subscription status';
+	}
+	me.update(data);
+    },
+    tpl: [
+	'<table style="height: 100%;" class="dash">',
+	'<tr><td class="center">',
+	'<i class="fa fa-3x fa-{icon}"></i>',
+	'</td><td class="center">{message}</td></tr>',
+	'</table>'
+    ],
+
+    listeners: {
+	click: {
+	    element: 'body',
+	    fn: function() {
+		var mainview = this.component.up('mainview');
+		mainview.getController().redirectTo('pmgSubscription');
+	    }
+	}
+    }
+})
