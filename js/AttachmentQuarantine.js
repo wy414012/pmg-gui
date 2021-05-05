61c1d4d5b9d9ee3216666e4e3c70769f37362dc7
Stoiko Ivanov <s.ivanov@proxmox.com>
2021-05-05 11:33:03 +0200

    attachmentquarantine: fix missing '&' for raw-param addition
    small glitch introduced in the code cleanup in:
    0e26e20aa5522b3f7d05e68c96104b051419b901
    
    Signed-off-by: Stoiko Ivanov <s.ivanov@proxmox.com>

--- js/AttachmentQuarantine.js
+++ js/AttachmentQuarantine.js
@@ -42,7 +42,7 @@ Ext.define('PMG.AttachmentQuarantine', {
 
 	    let url = `/api2/htmlmail/quarantine/content?id=${rec.data.id}`;
 	    if (raw) {
-		url += 'raw=1';
+		url += '&raw=1';
 	    }
 	    preview.setDisabled(false);
 	    preview.update("<iframe frameborder=0 width=100% height=100% sandbox='allow-same-origin' src='" + url +"'></iframe>");
