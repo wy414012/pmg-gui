wy414012/pmg-gui

commit cd0b3ffca8cfcd4fe7fd5b49e43eceaeea3a5ded

quarantines: mail filter: add clear trigger
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

js/QuarantineList.js
@@ -190,6 +190,7 @@ Ext.define('PMG.QuarantineList', {
 	    } else {
 		me.setUser(value);
 	    }
+	    tb.triggers.clear.setVisible(value.length > 0 && value !== 'all');
 	    me.load();
 	},
 
@@ -362,6 +363,18 @@ Ext.define('PMG.QuarantineList', {
 		selectOnFocus: true,
 		reference: 'email',
 		fieldLabel: 'E-Mail',
+		triggers: {
+		    clear: {
+			cls: 'pmx-clear-trigger',
+			weight: -1,
+			hidden: true,
+			handler: function() {
+			    this.triggers.clear.setVisible(false);
+			    // 'all' is unfiltered here, so empty/originalValue makes no sense
+			    this.setValue('all');
+			},
+		    },
+		},
 	    },
 	    {
 		xtype: 'textfield',

commit 5b920c359ebfb4b915c89ceb891f381981268482

quarantine list: cope with undefined mail value
can be (temporarily) null for some selections

Fixes: cd0b3ff ("quarantines: mail filter: add clear trigger")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

js/QuarantineList.js
@@ -190,7 +190,7 @@ Ext.define('PMG.QuarantineList', {
 	    } else {
 		me.setUser(value);
 	    }
-	    tb.triggers.clear.setVisible(value.length > 0 && value !== 'all');
+	    tb.triggers.clear.setVisible(value?.length > 0 && value !== 'all');
 	    me.load();
 	},
 